4ad6efdbe5f1ddb35c5b34f91e84265537992a7e
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-13 21:52:04 +0330

    displayNews method added to news class.

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -26,13 +26,22 @@ public class Infrastructure {
         private String publishedAt;
 
         public news(String title, String description, String sourceName, String author, String url, String publishedAt) {
-            
+
             this.title = title;
             this.description = description;
             this.sourceName = sourceName;
             this.author = author;
             this.url = url;
             this.publishedAt = publishedAt;
+        }
+
+        public void displayNews() {
+            System.out.println("Title: " + title);
+            System.out.println("Description: " + description);
+            System.out.println("Source: " + sourceName);
+            System.out.println("Author: " + author);
+            System.out.println("URL: " + url);
+            System.out.println("Published At: " + publishedAt);
         }
     }
 
