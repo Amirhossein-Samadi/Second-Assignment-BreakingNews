a8ff63825fb8796b887cdf98ad997d2576c3bee9
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-13 21:43:12 +0330

    news constructor added.

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -25,7 +25,15 @@ public class Infrastructure {
         private String url;
         private String publishedAt;
 
-        
+        public news(String title, String description, String sourceName, String author, String url, String publishedAt) {
+            
+            this.title = title;
+            this.description = description;
+            this.sourceName = sourceName;
+            this.author = author;
+            this.url = url;
+            this.publishedAt = publishedAt;
+        }
     }
 
 
