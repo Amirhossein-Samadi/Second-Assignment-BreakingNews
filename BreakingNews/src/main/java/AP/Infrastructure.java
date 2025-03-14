7e0e9ebdf505ff76e5840946cb25a051158459f5
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-14 17:52:48 +0330

    Updating News class.

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -21,39 +21,6 @@ public class Infrastructure {
     private final String JSONRESULT;
     private ArrayList<News> newsList; // TODO: Create the News class
 
-    public class News{
-
-        private String title;
-        private String description;
-        private String sourceName;
-        private String author;
-        private String url;
-        private String publishedAt;
-
-        public News(String title, String description, String sourceName, String author, String url, String publishedAt) {
-
-            this.title = title;
-            this.description = description;
-            this.sourceName = sourceName;
-            this.author = author;
-            this.url = url;
-            this.publishedAt = publishedAt;
-        }
-
-        public void displayNews() {
-            System.out.println("Title: " + title);
-            System.out.println("Description: " + description);
-            System.out.println("Source: " + sourceName);
-            System.out.println("Author: " + author);
-            System.out.println("URL: " + url);
-            System.out.println("Published At: " + publishedAt);
-        }
-
-        public String getTitle() {
-            return title;
-        }
-    }
-
 
     public Infrastructure(String APIKEY) {
         this.APIKEY = APIKEY;
