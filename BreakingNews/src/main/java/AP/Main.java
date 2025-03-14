300564f4f1c614c6567f83cc76ef5e0ba2867f47
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-14 09:57:50 +0330

    Menu was added to main.

--- BreakingNews/src/main/java/AP/Main.java
+++ BreakingNews/src/main/java/AP/Main.java
@@ -1,7 +1,12 @@
 package AP;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String apiKey = "YOUR_API_KEY_HERE";
+        Infrastructure infrastructure = new Infrastructure(apiKey);
 
+        infrastructure.displayNewsList();
     }
 }
